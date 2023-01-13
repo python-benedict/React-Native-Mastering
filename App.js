48b145420c11ed57beda6dfa386b4efe295d970e
Benedict <newlyf38@gmail.com>
2023-01-13 08:45:34 +0000

    created a button with touchable opacity that returns the user to youtube

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to the master react native project </Text>
+      <TouchableOpacity style={styles.button} onPress={()=>{Linking.openURL('https://www.youtube.com/')}}>
+        <Text style={styles.buttonText}>Youtube Buttons</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    width: '70%',
+    height: 40,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  buttonText:{
+    fontWeight: 'bold',
+    color: '#ffffff'
+  }
 });
